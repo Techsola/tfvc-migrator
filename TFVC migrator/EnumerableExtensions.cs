6e40203354df555cda3ba70fb8c59c7db6ceeed6
jnm2 <me@jnm2.com>
2020-04-18 21:17:15 -0400

    More unused extension methods

--- TFVC migrator/EnumerableExtensions.cs
+++ TFVC migrator/EnumerableExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -38,33 +37,6 @@ namespace TfvcMigrator
             where T : struct
         {
             return source.Where(predicate).FirstOrNull();
-        }
-
-        public static IEnumerable<T> Values<T>(this IEnumerable<T?> source)
-            where T : struct
-        {
-            foreach (var value in source)
-            {
-                if (value is { } unwrapped)
-                    yield return unwrapped;
-            }
-        }
-
-        public static int FindSingleIndex<T>(this ImmutableArray<T> array, Func<T, bool> predicate)
-        {
-            var index = -1;
-
-            for (var i = 0; i < array.Length; i++)
-            {
-                if (!predicate(array[i])) continue;
-
-                if (index == -1)
-                    index = i;
-                else
-                    throw new InvalidOperationException("The sequence contains more than one matching element.");
-            }
-
-            return index;
         }
     }
 }
