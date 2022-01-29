8570834840abdb8bfef950c470fdfb1bc6dda369
jnm2 <me@jnm2.com>
2022-01-29 14:15:11 -0500

    Warn for unnecessary usings

--- src/TfvcMigrator/Program.cs
+++ src/TfvcMigrator/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.CommandLine;
-using System.CommandLine.Invocation;
 using System.Globalization;
 using System.IO;
 using System.Linq;
