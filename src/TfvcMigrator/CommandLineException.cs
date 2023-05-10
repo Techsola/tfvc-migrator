8cfc92e280cde938c14bccff0ab5371bc21070cf
Daniel Vining <dvining@techsola.com>
2023-05-10 14:45:05 -0500

    Add command line exception

---
+++ src/TfvcMigrator/CommandLineException.cs
@@ -0,0 +1,9 @@
+﻿namespace TfvcMigrator;
+public sealed class CommandLineException : ApplicationException
+{
+    public int ExitCode { get; }
+    public CommandLineException(string message, int exitCode = 1) : base(message)
+    {
+        ExitCode = exitCode;
+    }
+}
