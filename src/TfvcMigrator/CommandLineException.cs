ec901b3d05e7dfb63b8ae2d66bee3c1d71ebcbfd
viningtechsola <132921713+viningtechsola@users.noreply.github.com>
2023-05-12 09:47:26 -0500

    Make CommandLineException inherit from Exception
    Co-authored-by: Joseph Musser <me@jnm2.com>

--- src/TfvcMigrator/CommandLineException.cs
+++ src/TfvcMigrator/CommandLineException.cs
@@ -1,5 +1,5 @@
 ﻿namespace TfvcMigrator;
-public sealed class CommandLineException : ApplicationException
+public sealed class CommandLineException : Exception
 {
     public int ExitCode { get; }
     public CommandLineException(string message, int exitCode = 1) : base(message)
